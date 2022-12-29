2b32dfddcf962dec5281e064fd7f4ab3c9e5fcea
Aurimas <bavarskiss@gmail.com>
2022-12-29 10:23:41 +0100

    Add empty XCode project

---
+++ PlacesSearch/PlacesSearchApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlacesSearchApp.swift
+//  PlacesSearch
+//
+//  Created by Aurimas Bavarskis on 29/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct PlacesSearchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
